02e850e8b2950ecb32bb4144182a3ed26486ed76
salvadormencias29-sys <salvadormencias.29@gmail.com>
2026-06-07 21:22:08 -0400

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,48 +1,20 @@
-window.addEventListener('DOMContentLoaded', () => {
-    /* VALIDACION DEL FORMULARIO DE RESERVAS */
+const formulario = document.getElementById('formReserva');
 
-    const form = document.querySelector('.form-reserva');
-    const nombre = document.getElementById('nombre');
-    const telefono = document.getElementById('telefono');
-    const correo = document.getElementById('correo');
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-    const modal = document.getElementById('modal-confirm');
-    const cerrarModal = document.getElementById('cerrar-modal');
+    const datosCita = {
+        nombre: document.getElementById('nombre').value,
+        telefono: document.getElementById('telefono').value,
+        correo: document.getElementById('correo').value,
+        servicio: document.getElementById('servicio').value,
+        barbero: document.getElementById('barbero').value,
+        fecha: document.getElementById('fecha').value,
+        hora: document.getElementById('hora').value,
+        mensaje: document.getElementById('mensaje').value || "Ninguno"
+    };
 
-    const phoneRegex = /^\d{8,9}$/;
-    const nameRegex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ]+(\s+[A-Za-zÁÉÍÓÚáéíóúÑñ]+)+$/;
-    const gmailRegex = /\S+@\S+\.\S+/;
+    localStorage.setItem('reservaActual', JSON.stringify(datosCita));
 
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-
-        let nombreVal = nombre.value.trim();
-        if(!nameRegex.test(nombreVal)){
-            alert('Por favor ingresa tu nombre completo.');
-            nombre.focus();
-            return;
-        }
-
-        let telefonoVal = telefono.value.trim();
-        if(!phoneRegex.test(telefonoVal)){
-            alert('Número de teléfono inválido. Usa solo dígitos [0-9]');
-            telefono.focus();
-            return;
-        }
-
-        let correoVal = correo.value.trim();
-        if(!gmailRegex.test(correoVal)){
-            alert('Ingresa correctamente tu correo electrónico');
-            correo.focus();
-            return;
-        }
-
-        // Mostrar modal
-        modal.classList.add("activo");
-    });
-
-    cerrarModal.addEventListener('click', () => {
-        modal.classList.remove('activo');
-        form.reset();
-    });
+    window.location.href = "../html/reserva.html";
 });
